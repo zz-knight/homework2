65a55ddf27eb672a8e1b46003e946553edfb089e
Zhongyin Zhao <1027639479@qq.com>
2020-07-14 22:05:36 +0800

    single change

--- use_naive_bayes.py
+++ use_naive_bayes.py
@@ -12,11 +12,11 @@ Y=mnist["target"]
 X = X / 255.
 X_train, X_test, Y_train, Y_test = train_test_split(X[::10], Y[::10], test_size=1000)
 
-bl=BL()
-bl.fit(X_train,Y_train)
+berl=BL()
+berl.fit(X_train,Y_train)
 
-train_accuracy=bl.score(X_train,Y_train)
-test_accuracy=bl.score(X_test,Y_test)
+train_accuracy=berl.score(X_train,Y_train)
+test_accuracy=berl.score(X_test,Y_test)
 
 print('Training accuracy: %0.2f%%' % (train_accuracy*100))
 print('Testing accuracy: %0.2f%%' % (test_accuracy*100))
